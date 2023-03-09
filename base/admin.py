c68105148211fdaef4f619d8bb400c5b83583435
Uday kiran <udaykiran.9114@gmail.com>
2023-03-09 19:28:57 +0530

    first commit

---
+++ base/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from . models import Room,Topic,Messages
+
+# Register your models here.
+
+admin.site.register(Room)
+admin.site.register(Topic)
+admin.site.register(Messages)
